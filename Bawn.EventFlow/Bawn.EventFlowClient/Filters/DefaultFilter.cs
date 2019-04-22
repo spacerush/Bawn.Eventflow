5d7dc6f8d61d21636582d44fe826785f59a3398f
Philip <philip@spacerush.app>
2019-04-22 02:10:38 -0700

    An example program for future reference.

---
+++ Bawn.EventFlow/Bawn.EventFlowClient/Filters/DefaultFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Diagnostics.EventFlow.ApplicationInsights;
+using Microsoft.Diagnostics.EventFlow.HealthReporters;
+using Microsoft.Diagnostics.EventFlow.Inputs;
+using Microsoft.Diagnostics.EventFlow.Outputs;
+using Microsoft.Diagnostics.EventFlow.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bawn.EventFlowClient
+{
+    public class DefaultFilter : IFilter
+    {
+        private IHealthReporter HealthReporter;
+        private string MachineName;
+        public DefaultFilter(string ServerName, IHealthReporter HealthReporter)
+        {
+            MachineName = ServerName;
+            this.HealthReporter = HealthReporter;
+        }
+
+        FilterResult IFilter.Evaluate(EventData eventData)
+        {
+            // By default, my ApplicationInsightsIngester accepts new telemetry posted to an endpoint that ends with /api/Collect.so i'm not interested in 
+            // If I were to record telemetry about this dependency there would be an continuous
+            // loop of telemetry recorded about telemetry recorded about telemetry recorded about telemetry... ..you get the idea.
+            if (eventData.Payload["TelemetryType"].ToString() == "dependency" && eventData.Payload["Name"].ToString().StartsWith("POST") && eventData.Payload["Name"].ToString().EndsWith("/api/Collect"))
+            {
+                return FilterResult.DiscardEvent;
+            }
+            else
+            {
+                eventData.AddPayloadProperty("ServerName", MachineName, HealthReporter, "CustomFilter");
+                return FilterResult.KeepEvent;
+            }
+        }
+    }
+}
---
+++ Bawn.EventFlow/Bawn.EventFlowClient/Filters/DefaultFilterFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bawn.EventFlowClient
+{
+    public class DefaultFilterFactory : IPipelineItemFactory<DefaultFilter>
+    {
+        public DefaultFilter CreateItem(IConfiguration configuration, IHealthReporter healthReporter)
+        {
+            DefaultFilter filter = new DefaultFilter(System.Environment.MachineName, healthReporter);
+            return filter;
+        }
+    }
+}
